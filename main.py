a39f1fed319ce27cfc83ea1cb4ddb239ff9de968
TheUltimateOptimist <digitalConfidence@web.de>
2021-10-27 22:04:03 +0200

    added befehl dokumentation

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from calculate import calculate
 from aussage import Aussage
 from rich.text import Text
 from my_console import console
-from language import erklaerung
-def erklaere():
-    console.print(Text("\n".join(erklaerung.split("-n-")), "bold cyan"))
+from language import erklaerung, befehle
 
 def evaluateInput(text):
     if text == "erkläre":
-        erklaere()
+        console.print(Text("\n".join(erklaerung.split("-n-")), "bold cyan"))
+    elif text == "befehle":
+        console.print(Text("\n".join(befehle.split("-n-")), "bold magenta"))
     elif text == "tafel":
         printTafel()
     elif text == "binär":
@@ -38,6 +38,7 @@ def welcomeUser():
     console.print(Text("Source Code: ").append(link))
     console.print(Text("Wilkommen!", "bold green"))
     console.print(Text("Um zu erfahren wie dieses Tool funktioniert, gib 'erkläre' ein, und drücke Enter!", "bold green"))
+    console.print(Text("Um alle Befehle anzeigen zu lassen, gib 'befehle' ein, und drücke Enter!", "bold green"))
 
 if __name__ == '__main__':
     welcomeUser()
